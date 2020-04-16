d72386056e4baec49519778816bc3f0457b8dc0e
Erol Gorancic <erolg@protonmail.com>
2020-04-16 16:08:23 +0200

    converted to springboot application

---
+++ src/main/java/no/netb/magnetar/MagnetarApplication.java
@@ -0,0 +1,13 @@
+package no.netb.magnetar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagnetarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagnetarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/no/netb/magnetar/MagnetarApplicationTests.java
@@ -0,0 +1,13 @@
+package no.netb.magnetar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagnetarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
